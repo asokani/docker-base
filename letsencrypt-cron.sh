2823b175f92183a30e45f6a8cb570d975292c2a1
asokani <1385293+asokani@users.noreply.github.com>
2020-08-19 18:12:28 +0200

    Update letsencrypt-cron.sh

--- letsencrypt-cron.sh
+++ letsencrypt-cron.sh
@@ -4,7 +4,7 @@ then
   SIGNED_CRT="$(mktemp --suffix=signed_crt)"
   sudo -u acme date >> /var/log/acme/acme_tiny.log
   sudo -u acme /usr/bin/python /opt/acme_tiny.py --account-key /etc/secrets-global/letsencrypt/letsencrypt-account.key --csr /etc/secrets/letsencrypt/domain.csr --acme-dir /var/app-cert/.well-known/acme-challenge > $SIGNED_CRT 2>> /var/log/acme/acme_tiny.log
-  if [ -s $SIGNED_CRT ] && /usr/bin/openssl verify $SIGNED_CRT
+  if [ -s $SIGNED_CRT ] # && /usr/bin/openssl verify $SIGNED_CRT
   then
     chmod 400 $SIGNED_CRT
     mv $SIGNED_CRT /etc/secrets/letsencrypt/signed.crt
